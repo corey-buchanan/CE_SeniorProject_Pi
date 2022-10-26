1ab9aaed9a8fc1788e46314fb5e3ef6140d12c56
Corey Buchanan <coreybtechdog@gmail.com>
2022-10-25 23:07:23 -0600

    Fix video stream

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sleep 5
-raspivid -n -t 0 -w 1280 -h 720 -ih -fps 24 -fl -o - | nc -lkv4 8080 &
+sleep 10
+# raspivid -n -t 0 -w 1280 -h 720 -ih -fps 24 -fl -o - | nc -lkv4 8080 &
+python3 /home/drone/drone/video_stream/stream_server.py &
 
-python3 /home/drone/drone/servo/servo_ctrl.py 8001
+python3 /home/drone/drone/servo/servo_ctrl.py 8001 &
