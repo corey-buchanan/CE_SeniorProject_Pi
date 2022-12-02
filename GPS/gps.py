f1ad7bb2dc2f57e7d58b38edde5642aea5b7b44f
Vanessa Bentley <42619074+vanessa-bentley@users.noreply.github.com>
2022-12-01 22:30:49 -0700

    Update gps.py
    This is a working version of the gps code

--- GPS/gps.py
+++ GPS/gps.py
@@ -1,25 +1,78 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Simple GPS module demonstration.
+# Will wait for a fix and print a message every second with the current location
+# and other details.
+import time
+import board
+import busio
+import adafruit_gps
+
+# Create a serial connection for the GPS connection using default speed and
+# a slightly higher timeout (GPS modules typically update once a second).
+
 import serial
+uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=10)
 
-Import time
+# Create a GPS module instance.
+gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
+# gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)  # Use I2C interface
 
-import string import pynmea2
+# Initialize the GPS module by changing what data it sends and at what rate.
 
-while True: port=“/dev/ttyAMAO”
+# Turn on the basic GGA and RMC info 
+gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
 
-ser=serial.Serial(port,baudrate=9600,timeout=0.5)
 
-dataout =pynmea2.NMEAStreamReader()
+# Set update rate to once a second (1hz) 
+gps.send_command(b"PMTK220,1000")
 
-newdata=ser.readline()
 
-if newdata[0:6]==“$GPRMC”:
+# Main loop runs forever printing the location, etc. every second.
+last_print = time.monotonic()
+while True:
+    # Make sure to call gps.update() every loop iteration and at least twice
+    # as fast as data comes from the GPS unit (usually every second).
+    # This returns a bool that's true if it parsed new data (you can ignore it
+    # though if you don't care and instead look at the has_fix property).
+    gps.update()
+    # Every second print out current location details if there's a fix.
+    current = time.monotonic()
+    if current - last_print >= 1.0:
+        last_print = current
+        if not gps.has_fix:
+            # Try again if we don't have a fix yet.
+            print("Waiting for fix...")
+            continue
+        # We have a fix! (gps.has_fix is true)
+        # Print out details about the fix like location, date, etc.
+        print("=" * 40)  # Print a separator line.
+        print(
+            "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
+                gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
+                gps.timestamp_utc.tm_mday,  # struct_time object that holds
+                gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+                gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+                gps.timestamp_utc.tm_min,  # month!
+                gps.timestamp_utc.tm_sec,
+            )
+        )
+        print("Latitude: {0:.6f} degrees".format(gps.latitude))
+        print("Longitude: {0:.6f} degrees".format(gps.longitude))
 
-newmsg=pynmea2.parse(newdata)
-
-lat=newmsg.latitude
-
-lng=newmsg.longitude
-
-gps=“Latitude=" +str(lat) + “and Longitude=" +str(lng)
-
-print(gps)
+        print("Fix quality: {}".format(gps.fix_quality))
+        # Some attributes beyond latitude, longitude and timestamp are optional
+        # and might not be present.  Check if they're None before trying to use!
+#        if gps.satellites is not None:
+#            print("# satellites: {}".format(gps.satellites))
+        if gps.altitude_m is not None:
+            print("Altitude: {} meters".format(gps.altitude_m))
+        if gps.speed_knots is not None:
+            print("Speed: {} knots".format(gps.speed_knots))
+#        if gps.track_angle_deg is not None:
+#            print("Track angle: {} degrees".format(gps.track_angle_deg))
+#        if gps.horizontal_dilution is not None:
+#            print("Horizontal dilution: {}".format(gps.horizontal_dilution))
+#        if gps.height_geoid is not None:
+#            print("Height geoid: {} meters".format(gps.height_geoid))
