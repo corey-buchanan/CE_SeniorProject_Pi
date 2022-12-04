c5404a0f8b729dba5ec5c999c3cac497899ea0cd
Corey Buchanan <coreybtechdog@gmail.com>
2022-12-03 21:03:51 -0700

    Add pifi reset to startup.

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+service dhcpcd stop
+sleep 5
+service hostapd stop
+sleep 5
+service hostapd start
+sleep 5
+service dhcpcd start
+
 sleep 10
 # raspivid -n -t 0 -w 1280 -h 720 -ih -fps 24 -fl -o - | nc -lkv4 8080 &
 python3 /home/drone/drone/video_stream/stream_server.py &
