f4dd42837949ea49945a4ada7f294e4338545409
Corey Buchanan <coreybtechdog@gmail.com>
2022-12-07 14:22:14 -0700

    Gps code

--- GPS/gps.py
+++ GPS/gps.py
@@ -1,9 +1,11 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
-
+#
+# Edited Dec 2022 for use with University of Utah Casper Drone Project
+# Vanessa Bentley
+#
 # Simple GPS module demonstration.
-# Will wait for a fix and print a message every second with the current location
-# and other details.
+
 import time
 import board
 import busio
@@ -17,10 +19,8 @@ uart = serial.Serial("/dev/serial0", baudrate=9600, timeout=10)
 
 # Create a GPS module instance.
 gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
-# gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)  # Use I2C interface
 
 # Initialize the GPS module by changing what data it sends and at what rate.
-
 # Turn on the basic GGA and RMC info 
 gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
 
@@ -47,29 +47,28 @@ while True:
             continue
         # We have a fix! (gps.has_fix is true)
         # Print out details about the fix like location, date, etc.
-        print("=" * 40)  # Print a separator line.
-        print(
-            "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
-                gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
-                gps.timestamp_utc.tm_mday,  # struct_time object that holds
-                gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-                gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-                gps.timestamp_utc.tm_min,  # month!
-                gps.timestamp_utc.tm_sec,
-            )
-        )
+        print("-" * 40)  # Print a separator line.
+#        print(
+#            "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
+#                gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
+#                gps.timestamp_utc.tm_mday,  # struct_time object that holds
+#                gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+#                gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+#                gps.timestamp_utc.tm_min,  # month!
+#                gps.timestamp_utc.tm_sec,
+#            )
+#        )
         print("Latitude: {0:.6f} degrees".format(gps.latitude))
         print("Longitude: {0:.6f} degrees".format(gps.longitude))
 
-        print("Fix quality: {}".format(gps.fix_quality))
-        # Some attributes beyond latitude, longitude and timestamp are optional
-        # and might not be present.  Check if they're None before trying to use!
+#        print("Fix quality: {}".format(gps.fix_quality))
+
 #        if gps.satellites is not None:
 #            print("# satellites: {}".format(gps.satellites))
-        if gps.altitude_m is not None:
-            print("Altitude: {} meters".format(gps.altitude_m))
-        if gps.speed_knots is not None:
-            print("Speed: {} knots".format(gps.speed_knots))
+#        if gps.altitude_m is not None:
+#            print("Altitude: {} meters".format(gps.altitude_m))
+#        if gps.speed_knots is not None:
+#            print("Speed: {} knots".format(gps.speed_knots))
 #        if gps.track_angle_deg is not None:
 #            print("Track angle: {} degrees".format(gps.track_angle_deg))
 #        if gps.horizontal_dilution is not None:
